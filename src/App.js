225f67ad93363fb0a3d6332f6c3b98395469b710
Burke Holland <burkeholland@gmail.com>
2019-03-29 17:08:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = { lead: "", punch: "" };
+
+  async componentDidMount() {
+    const result = await axios.get("/api/HttpTrigger");
+    this.setState({ lead: result.data.lead, punch: result.data.punch });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <h1>{this.state.lead}</h1>
+          <p>{this.state.punch}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
